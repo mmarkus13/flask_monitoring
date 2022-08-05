d763a6a6f12e4c249a88d594f33cb57549f696fd
mmarkus13 <mmarkus13@users.noreply.github.com>
2022-08-05 12:08:04 +0200

    Update monitoring_services.sh

--- monitoring_services.sh
+++ monitoring_services.sh
@@ -1,4 +1,10 @@
-#!/bin/bash -x
+# create backend script:
+vi monitoring_services.sh
+# paste the below (to be updated) #current version = v1.05; 2022.08.05 (Author: Michal Márkus)
+########################################################################################
+
+#!/bin/bash 
+# -x
 # monitoring_services.sh
 
 
@@ -20,7 +26,6 @@ services_check()
         {
                 grafana_check="$(curl -sL -I itahdnasrep.bmwgroup.net:3000/ping/api/health | grep HTTP | grep 200 | awk '{print $2}')";  # echo "$grafana_check"
                 grafana_latency=$(curl -s -w 'Establish Connection: %{time_connect}s\nTTFB: %{time_starttransfer}s\nTotal: %{time_total}s\n' itahdnasrep.bmwgroup.net:3000/ping/api/health | egrep "Total: [1-9]");  # echo $grafana_latency  # test with 0
-                #[ $(eval echo \$"${service}_check") == 200 ] && echo "${service} OK" || echo "${service}" not running $DATE
                 [ $(eval echo \$"${service}_check") == 200 ] && [ -z "$latency" ] || echo -e "\n$DATE\n$grafana_check\n\n###" >> ${service}_high_latency.log && echo "${service} OK" >> ${service}_uptime.log || echo "${service}" $err_msg $DATE >> ${service}_uptime.log
         }
 
@@ -32,11 +37,8 @@ services_check()
 
         harvest_status()
         {
-#       service="harvest"
-
                 harvest_check="$(ssh -tt michal@itahdnasuathar.bmwgroup.net 'systemctl status harvest')"  # to be replaced with qq user!
                 if [ "$(eval echo \$${service}_check) | sort -u | grep -v running | wc -l)" -gt 0 ]; then echo "${service}" $err_msg $DATE >> ${service}_uptime.log; else echo "${service} OK";fi
-
         }
 
 
@@ -87,23 +89,17 @@ for((i=1;i<=5;++i))
     do
         dat=$(tac telegraf_uptime.log | sed -n "${i},1p" | cut -d@ -f2)
 
-        # with this solution when service is OK it can't have date...
-#       case $dat in
-#               ''|*[!0-9]) return 1 ;;
-#               *) echo good ;;
-#        esac
-
         case $dat in
-         ''|*OK) return 1 ;;
-#        *) echo good ;;
+            ''|*OK) return 1 ;;
+            #        *) echo  ;;
+
+            epoch_dat=`date -d "${dat}" +"%s"`
+
+            if [ "$(echo $EPOCHNOW-$epoch_dat|bc)" -le "360"  ] # less or equal to 360 seconds AKA 6 min (5min +1min grace time due to latency)
+                then c=$((c+1))
+                if [ "$c" == 1 ]; then echo ${service}_down_since "$epoch_dat"; fi
+            fi
         esac
-
-        epoch_dat=`date -d "${dat}" +"%s"`
-
-                if [ "$(echo $EPOCHNOW-$epoch_dat|bc)" -le "360"  ] # less or equal to 360 seconds AKA 6 min (5min +1min grace time due to latency)
-                    then c=$((c+1))
-                    if [ "$c" == 1 ]; then echo ${service}_down_since "$epoch_dat"; fi
-                fi
     done
 
 if [[ $c -ge 5 ]]; then echo "PLACEHOLDER for *ticket file with paramenters edited by sed will be passed to NodeRed*" | tee > monitoring_ticket_${DATE}.json; else echo OK; fi
