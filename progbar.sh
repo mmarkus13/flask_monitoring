d2dd22c33d21a65ac30ebffb8bb027542b05e85f
mmarkus13 <mmarkus13@users.noreply.github.com>
2022-09-22 22:51:41 +0200

    Add files via upload

--- progbar.sh
+++ progbar.sh
@@ -14,3 +14,21 @@ echo -ne '\n'
 echo -e '\e[1A\e[K '
 echo -e '\e[2A\e[K ...Completed!'
 echo
+
+###############################
+
+
+#!/bin/bash
+sleep 5 &
+pid=$!
+frames="/ | \\ -"
+while kill -0 $pid 2&>1 > /dev/null;
+do
+    for frame in $frames;
+    do
+        printf "\r$frame Loading..." 
+        sleep 0.5
+    done
+done
+printf "\n"
+
