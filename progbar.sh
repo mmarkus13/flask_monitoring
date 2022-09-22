7f447bd4286ded3781954cebedbddb71efe9593c
mmarkus13 <mmarkus13@users.noreply.github.com>
2022-09-22 19:26:13 +0200

    Add files via upload

---
+++ progbar.sh
@@ -0,0 +1,16 @@
+echo -e '\e[1A\e[K .....Checking services'
+sleep 1
+echo -ne '#####          (33%)\r'
+sleep 1
+
+echo -e '\e[1A\e[K ....Smashing Mushrooms'
+echo -ne '##########     (66%)\r'
+sleep 1
+
+echo -e '\e[1A\e[K ..........Writing Logs'
+echo -ne '###############(100%)\r'
+echo -ne '\n'
+
+echo -e '\e[1A\e[K '
+echo -e '\e[2A\e[K ...Completed!'
+echo
